ff93c036444d57537768ef743a02d3d6a93ba792
Pedro Paulo <0901dantaspedro@gmail.com>
2023-05-22 19:45:58 -0300

    Possible moves of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -10,7 +10,11 @@ public class King extends ChessPiece {
     }
 
     @Override
-    public String toString(){
-        return "K";
+    public String toString(){ return "K";}
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
     }
 }
